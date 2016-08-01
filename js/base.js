3b8e4e81d8cf70aa05e90cfba69434cba4595e31
raul <owen002@gmail.com>
2016-08-01 18:15:05 +0800

    index modify

--- js/base.js
+++ js/base.js
@@ -1,16 +1,45 @@
-var base = (function() {
-	var baseUrl = '/';
-	var api = {
+var base = (function () {
+    var baseUrl = '/';
+    var api = {};
 
-	};
+    function delay(callback, time) {
+        time = time || 500;
+        return setTimeout(callback, time);
+    }
 
-	function setPageRem() {
-		var cwidth = document.body.clientWidth;
-		document.getElementsByTagName("html")[0].style.fontSize = cwidth / 10 + 'px';
-	}
+    function setPageRem() {
+        var cwidth = document.body.clientWidth;
+        document.getElementsByTagName("html")[0].style.fontSize = cwidth / 10 + 'px';
+    }
 
-	return {
-		api: api,
-		setPageRem:setPageRem
-	}
+    function hasClass(dom, className) {
+        var classNames = dom.getAttribute('class');
+        if (classNames.indexOf(className) >= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function addClass(dom, className) {
+        var classNames = dom.getAttribute('class');
+        classNames += ' ' + className;
+        dom.setAttribute('class', classNames);
+    }
+
+    function removeClass(dom, className) {
+        var classNames = dom.getAttribute('class');
+        var reg = new RegExp(className, 'g');
+        classNames = classNames.replace(reg, '');
+        dom.setAttribute('class', classNames);
+    }
+
+    return {
+        api: api,
+        setPageRem: setPageRem,
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        delay: delay
+    }
 })()
--- js/index.js
+++ js/index.js
@@ -1,56 +1,131 @@
-(function() {
+(function () {
 
-	var page = {
-		init: function() {
-			mui.init();
-			base.setPageRem();
-			page.initSliderMenu();
-			//图片轮播
-			mui("#slider").slider({
-				interval: 3000
-			});
-			page.bind();
-		},
-		initSliderMenu: function() {
-			//侧滑容器父节点
-			var offCanvasWrapper = mui('#offCanvasWrapper');
-			//主界面容器
-			var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
-			//菜单容器
-			var offCanvasSide = document.getElementById("offCanvasSide");
+    var userinfo = '';
+    var page = {
+        init: function () {
+            mui.init();
+            base.setPageRem();
+            page.initSliderMenu();
+            //图片轮播
+            mui("#slider").slider({
+                interval: 3000
+            });
+            page.checkUser();
+            page.bind();
+        },
+        checkUser: function () {
+            if (!!userinfo) {
+                userinfo = decodeURIComponent(userinfo);
+                var uid = userinfo.memberID;
+                if (!!uid) {
+                    //已登录
+                    mui('#goToLogin')[0].style.display = 'none';
+                }
+            }
+        },
+        initSliderMenu: function () {
+            //侧滑容器父节点
+            var offCanvasWrapper = mui('#offCanvasWrapper');
+            //主界面容器
+            var offCanvasInner = offCanvasWrapper[0].querySelector('.mui-inner-wrap');
+            //菜单容器
+            var offCanvasSide = document.getElementById("offCanvasSide");
 
-			//侧滑容器的class列表，增加.mui-slide-in即可实现菜单移动、主界面不动的效果；
-			var classList = offCanvasWrapper[0].classList;
+            //侧滑容器的class列表，增加.mui-slide-in即可实现菜单移动、主界面不动的效果；
+            var classList = offCanvasWrapper[0].classList;
 
-			offCanvasSide.classList.remove('mui-transitioning');
-			offCanvasSide.setAttribute('style', '');
-			classList.add('mui-scalable');
+            offCanvasSide.classList.remove('mui-transitioning');
+            offCanvasSide.setAttribute('style', '');
+            classList.add('mui-scalable');
 //			classList.add('mui-slide-in');
-			//主界面‘显示侧滑菜单’按钮的点击事件
-			document.getElementById('offCanvasShow').addEventListener('tap', function() {
-				var pageObj={
-					pageUrl:"page/login/login.html"
-				};
-				pageChange(pageObj);
-//				offCanvasWrapper.offCanvas('show');
-			});
-			//主界面和侧滑菜单界面均支持区域滚动；
-			mui('#offCanvasSideScroll').scroll();
-			mui('#offCanvasContentScroll').scroll();
-			//实现ios平台原生侧滑关闭页面；
-			if(mui.os.plus && mui.os.ios) {
-				mui.plusReady(function() { //5+ iOS暂时无法屏蔽popGesture时传递touch事件，故该demo直接屏蔽popGesture功能
-					plus.webview.currentWebview().setStyle({
-						'popGesture': 'none'
-					});
-				});
-			}
-		},
-		bind:function(){
-			mui("#gussULikeUl").on('tap','li',function(){
-				location.href='page/rent.html';
-			})
-		}
-	}
-	page.init();
+            //主界面‘显示侧滑菜单’按钮的点击事件
+            document.getElementById('offCanvasShow').addEventListener('tap', function () {
+                // var pageObj={
+                // 	pageUrl:"page/login/login.html"
+                // };
+                // pageChange(pageObj);
+                offCanvasWrapper.offCanvas('show');
+            });
+            //主界面和侧滑菜单界面均支持区域滚动；
+            mui('#offCanvasSideScroll').scroll();
+            mui('#offCanvasContentScroll').scroll();
+
+
+            //实现ios平台原生侧滑关闭页面；
+            if (mui.os.plus && mui.os.ios) {
+                mui.plusReady(function () { //5+ iOS暂时无法屏蔽popGesture时传递touch事件，故该demo直接屏蔽popGesture功能
+                    plus.webview.currentWebview().setStyle({
+                        'popGesture': 'none'
+                    });
+                });
+            }
+        },
+        bind: function () {
+            mui("#gussULikeUl").on('tap', 'li', function () {
+                location.href = 'page/rent.html';
+            });
+            mui(document).on('tap', '#goToLogin', function () {
+                var dom = this;
+                if (!base.hasClass(dom, 'go-to-login-active')) {
+                    base.addClass(dom, 'go-to-login-active');
+                }
+                base.delay(function () {
+                    if (base.hasClass(dom, 'go-to-login-active')) {
+                        base.removeClass(dom, 'go-to-login-active');
+                    }
+                }, 2000);
+                var pageObj = {
+                    pageUrl: "page/login/login.html"
+                };
+                pageChange(pageObj);
+            }).on('tap', '#navigateMenus li', function () {
+                var id = this.getAttribute('data-type');
+                var pageObj = {};
+                if (!userinfo) {
+                    switch (id) {
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                            pageObj = {
+                                pageUrl: "page/login/login.html"
+                            };
+                            pageChange(pageObj);
+                            break;
+                    }
+                } else {
+                    switch (id) {
+                        case '1':
+                            pageObj = {
+                                pageUrl: ""
+                            };
+                            break;
+                        case '2':
+                            pageObj = {
+                                pageUrl: ""
+                            };
+                            break;
+                        case '3':
+                            pageObj = {
+                                pageUrl: ""
+                            };
+                            break;
+                        case '4':
+                            pageObj = {
+                                pageUrl: ""
+                            };
+                            break;
+                        case '5':
+                            pageObj = {
+                                pageUrl: ""
+                            };
+                            break;
+                    }
+                    pageChange(pageObj);
+                }
+            });
+        }
+    }
+    page.init();
 })()

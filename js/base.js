5d0402ab643047bc01eae0dc07e841fdb9ca15f4
raul <owen002@gmail.com>
2016-08-02 09:58:52 +0800

    rentlist modify

--- js/base.js
+++ js/base.js
@@ -41,41 +41,106 @@ var base = (function () {
         mui('.mui-back')[0].style.top = 1.2 * baseRemSize + 'px';
     }
 
-    var height_0, maxHeight0;
+    var Slider = (function() {
+        var Slider = {};
+        function TimerManager() {
+            this.timers = [];
+            this.args = [];
+            this.isFiring = false;
+        }
 
-    function slideDown(obj) {
-        obj.style.display = 'block';
-        maxHeight0 = obj.offsetHeight;
-        obj.style.height = 0;
-        height_0 = 0;
-
-        var stime = setInterval(function () {
-            height_0 += 10;
-            if (height_0 < maxHeight0) {
-                obj.style.height = height_0 + 'px';
-            } else {
-                clearInterval(stime);
+        TimerManager.makeInstance = function(element) {
+            if (!element.__TimerManager__ || element.__TimerManager__.constructor != TimerManager) {
+                element.__TimerManager__ = new TimerManager();
             }
-        }, 10)
-    }
+        };
 
-    var height_1, maxHeight1;
+        TimerManager.prototype.add = function(timer, args) {
+            this.timers.push(timer);
+            this.args.push(args);
+            this.fire();
+        };
 
-    function slideUp(obj) {
-        maxHeight1 = obj.clientHeight;
-        height_1 = maxHeight1;
-
-        var stime = setInterval(function () {
-            height_1 -= 10;
-            if (height_1 > 0) {
-                obj.style.height = height_1 + 'px';
-            } else {
-                obj.style.height = maxHeight1 + 'px';
-                obj.style.display = 'none';
-                clearInterval(stime);
+        TimerManager.prototype.fire = function() {
+            if ( !this.isFiring ) {
+                var timer = this.timers.shift(),        // 取出定时器
+                    args  = this.args.shift();          // 取出定时器参数
+                if (timer && args) {
+                    this.isFiring = true;
+                    timer(args[0], args[1]);
+                }
             }
-        }, 10)
-    }
+        };
+
+        TimerManager.prototype.next = function() {
+            this.isFiring = false;
+            this.fire();
+        };
+
+        function fnSlideDown(element, time) {
+            if (element.offsetHeight == 0) {
+                // 获取元素总高度
+                element.style.display = "block";
+                var totalHeight = element.offsetHeight;
+                element.style.height = "0px";
+                var currentHeight = 0;
+                var increment = totalHeight / (time/10);
+                var timer = setInterval(function () {
+                    currentHeight = currentHeight + increment;
+                    element.style.height = currentHeight + "px";
+                    if (currentHeight >= totalHeight) {
+                        clearInterval(timer);
+                        element.style.height = totalHeight + "px";
+                        if (element.__TimerManager__ && element.__TimerManager__.constructor == TimerManager) {
+                            element.__TimerManager__.next();
+                        }
+                    }
+                }, 10);
+            } else {
+                if (element.__TimerManager__ && element.__TimerManager__.constructor == TimerManager) {
+                    element.__TimerManager__.next();
+                }
+            }
+        }
+
+        function fnSlideUp(element, time) {
+            if (element.offsetHeight > 0) {
+                var totalHeight = element.offsetHeight;
+                var currentHeight = totalHeight;
+                var decrement = totalHeight / (time/10);
+                var timer = setInterval(function() {
+                    currentHeight = currentHeight - decrement;
+                    element.style.height = currentHeight + "px";
+                    if (currentHeight <= 0) {
+                        clearInterval(timer);
+                        element.style.display = "none";
+                        element.style.height = totalHeight + "px";
+                        if (element.__TimerManager__ && element.__TimerManager__.constructor == TimerManager) {
+                            element.__TimerManager__.next();
+                        }
+                    }
+                }, 10);
+            } else {
+                if (element.__TimerManager__ && element.__TimerManager__.constructor == TimerManager) {
+                    element.__TimerManager__.next();
+                }
+            }
+        }
+
+        Slider.slideDown = function(element, time) {
+            TimerManager.makeInstance(element);
+            element.__TimerManager__.add(fnSlideDown, arguments);
+            return this;
+        };
+
+        Slider.slideUp = function(element, time) {
+            TimerManager.makeInstance(element);
+            element.__TimerManager__.add(fnSlideUp, arguments);
+            return this;
+        };
+
+        return Slider;
+    })();
 
     return {
         api: api,
@@ -85,7 +150,6 @@ var base = (function () {
         removeClass: removeClass,
         delay: delay,
         setMuiBackHeight: setMuiBackHeight,
-        slideDown: slideDown,
-        slideUp:slideUp
+        Slider:Slider
     }
-})()
+})();
